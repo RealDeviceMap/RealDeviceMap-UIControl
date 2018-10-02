09c7ba591ccf03cd62bed76cfc2f0a2c6a672219
Florian Kostenzer <florian.kostenzer@icloud.com>
2018-10-02 02:13:11 +0200

    use xcworkspace in run.py

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import subprocess
 import shlex
 
-device_id = 'DEVICE_UUID'
+device_id = '57c2eb15ea1f3267757892b65ae8d960528b3c33'
 
 while True:
 
-    process = subprocess.Popen(shlex.split('xcodebuild test -scheme "RealDeviceMap-UIControl" -destination "id={}" -allowProvisioningUpdates'.format(device_id)), stdout=subprocess.PIPE)
+    process = subprocess.Popen(shlex.split('xcodebuild test -workspace RealDeviceMap-UIControl.xcworkspace -scheme "RealDeviceMap-UIControl" -destination "id={}" -allowProvisioningUpdates'.format(device_id)), stdout=subprocess.PIPE)
     while True:
         output = process.stdout.readline()
         if output == '' and process.poll() is not None:
