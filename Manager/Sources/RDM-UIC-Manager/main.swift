0de862718d1e5e8603a43f96dea00b55dada9e5a
Florian Kostenzer <florian.kostenzer@icloud.com>
2018-12-06 14:04:31 +0100

    add --help, -h flag; set default timeout to 300s; set default builds to 2

--- Manager/Sources/RDM-UIC-Manager/main.swift
+++ Manager/Sources/RDM-UIC-Manager/main.swift
@@ -10,6 +10,17 @@ import PerfectLib
 import PerfectThread
 import SQLiteStORM
 
+if CommandLine.arguments.contains("--help") || CommandLine.arguments.contains("-help") || CommandLine.arguments.contains("-h") {
+    print("""
+    The following flags are avilable:
+      `-path path` (default = "..") [The Path to the Folder where UIC is at.]
+      `-derivedDataPath` path (default = "./DerivedData") [The Path to the DerivedData folder.]
+      `-timeout seconds` (default = 300) [Restart after x seconds if a test doesn't print anything.]
+      `-builds count` (default = 2) [Max synchronous builds.]
+    """)
+    exit(0)
+}
+
 let logsFolder = Dir("./logs")
 if !logsFolder.exists {
     do {
@@ -46,13 +57,13 @@ let timeout: Int
 if let index = CommandLine.arguments.index(of: "-timeout"), CommandLine.arguments.count > index + 1, let number = Int(CommandLine.arguments[index + 1]) {
     timeout = number
 } else {
-    timeout = 150
+    timeout = 300
 }
 let builds: Int
 if let index = CommandLine.arguments.index(of: "-builds"), CommandLine.arguments.count > index + 1, let number = Int(CommandLine.arguments[index + 1]) {
     builds = number
 } else {
-    builds = 1
+    builds = 2
 }
 // Start BuildController
 BuildController.global.start(path: path, derivedDataPath: derivedDataPath, timeout: timeout, maxSimultaneousBuilds: builds)
