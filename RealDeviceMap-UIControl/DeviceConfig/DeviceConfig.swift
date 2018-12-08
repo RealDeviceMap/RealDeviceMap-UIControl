3c3ba923dcee99bf995fb084ce705f9dcfe21b41
Drunk3nSlang <adamleveille1@gmail.com>
2018-12-08 03:30:17 +0000

    Update RealDeviceMap-UIControl/DeviceConfig/DeviceConfig.swift

---
+++ RealDeviceMap-UIControl/DeviceConfig/DeviceConfig.swift
@@ -0,0 +1,35 @@
+//
+//  DeviceConfig.swift
+//  RealDeviceMap-UIControlUITests
+//
+//  Created by Florian Kostenzer on 18.11.18.
+//
+
+import Foundation
+import XCTest
+
+class DeviceConfig {
+    
+    public static private(set) var global: DeviceConfigProtocol!
+    
+    public static func setup(app: XCUIApplication) {
+        let ratio = Int(app.frame.size.height / app.frame.size.width * 1000)
+        if ratio >= 1770 && ratio <= 1780 { // iPhones
+            switch app.frame.size.width {
+            case 375: // iPhone Normal
+                // This has no use and is an example only
+                global = DeviceIPhoneNormal(width: Int(app.frame.size.width), height: Int(app.frame.size.height))
+            case 414: // iPhone Large
+                global = DeviceRatio1775(width: Int(app.frame.size.width), height: Int(app.frame.size.height), multiplier: 1.5)
+            default: // other iPhones
+                global = DeviceRatio1775(width: Int(app.frame.size.width), height: Int(app.frame.size.height))
+            }
+        } else if ratio >= 1330 && ratio <= 1340 { // iPad
+            global = DeviceRatio1333(width: Int(app.frame.size.width), height: Int(app.frame.size.height))
+        } else {
+            Log.error("Unsuported Device")
+            fatalError("Unsuported Device")
+        }
+    }
+    
+}
