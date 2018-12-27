7ffaf6c10bcf77bd5acd20659489647b1ca01cb1
mzsmakr <AkiraMizushima@outlook.com>
2018-12-26 20:19:28 -0600

    New ban message fix for SE(other iPhone untested)

--- RealDeviceMap-UIControl/DeviceConfig/DeviceRatio1775.swift
+++ RealDeviceMap-UIControl/DeviceConfig/DeviceRatio1775.swift
@@ -176,11 +176,11 @@ class DeviceRatio1775: DeviceConfigProtocol {
     }
     
     var loginBannedText: DeviceCoordinate {
-        return DeviceCoordinate(x: 228, y: 479, scaler: scaler)
+        return DeviceCoordinate(x: 320, y: 700, scaler: scaler)
     }
     
     var loginBanned: DeviceCoordinate {
-        return DeviceCoordinate(x: 320, y: 600, scaler: scaler)
+        return DeviceCoordinate(x: 320, y: 575, scaler: scaler)
     }
     
     var loginBannedSwitchAccount: DeviceCoordinate {
--- RealDeviceMap-UIControl/RealDeviceMap_UIControlUITests.swift
+++ RealDeviceMap-UIControl/RealDeviceMap_UIControlUITests.swift
@@ -238,7 +238,25 @@ class RealDeviceMap_UIControlUITests: XCTestCase {
             var count = 0
             while !loaded {
                 let screenshotComp = XCUIScreen.main.screenshot()
-                if screenshotComp.rgbAtLocation(
+                if (screenshotComp.rgbAtLocation(
+                    pos: deviceConfig.loginBanned,
+                    min: (red: 0.0, green: 0.75, blue: 0.55),
+                    max: (red: 1.0, green: 0.90, blue: 0.70)) &&
+                    screenshotComp.rgbAtLocation(
+                        pos: deviceConfig.loginBannedText,
+                        min: (red: 0.0, green: 0.75, blue: 0.55),
+                        max: (red: 1.0, green: 0.90, blue: 0.70))
+                    ) {
+                    Log.error("Account \(username!) is banned.")
+                    username = nil
+                    isLoggedIn = false
+                    deviceConfig.loginBannedSwitchAccount.toXCUICoordinate(app: app).tap()
+                    postRequest(url: backendControlerURL, data: ["uuid": config.uuid, "username": self.username as Any, "type": "account_banned"], blocking: true) { (result) in }
+                    sleep(7 * config.delayMultiplier)
+                    shouldExit = true
+                    return
+                }
+                else if screenshotComp.rgbAtLocation(
                     pos: self.deviceConfig.startup,
                     min: (red: 0.0, green: 0.75, blue: 0.55),
                     max: (red: 1.0, green: 0.90, blue: 0.70)) {
@@ -383,8 +401,8 @@ class RealDeviceMap_UIControlUITests: XCTestCase {
                         max: (red: 1.0, green: 0.90, blue: 0.70)) &&
                         screenshotComp.rgbAtLocation(
                             pos: deviceConfig.loginBannedText,
-                            min: (red: 0.0, green: 0.0, blue: 0.0),
-                            max: (red: 0.3, green: 0.5, blue: 0.5))
+                            min: (red: 0.0, green: 0.75, blue: 0.55),
+                            max: (red: 1.0, green: 0.90, blue: 0.70))
                     ) {
                     Log.error("Account \(username!) is banned.")
                     username = nil
