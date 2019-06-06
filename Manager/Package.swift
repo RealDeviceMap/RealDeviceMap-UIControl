9c0386ad484427915df5385ef8cf5b5df5ed3578
Florian Kostenzer <florian.kostenzer@icloud.com>
2019-06-06 09:49:33 +0200

    Use .upToNextMinor in Package.swift

--- Manager/Package.swift
+++ Manager/Package.swift
@@ -6,8 +6,8 @@ let package = Package(
     name: "RDM-UIC-Manager",
     products: [],
     dependencies: [
-        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.18"),
-        .package(url: "https://github.com/SwiftORM/SQLite-StORM.git", from: "3.1.0")
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", .upToNextMinor(from: "3.0.18")),
+        .package(url: "https://github.com/SwiftORM/SQLite-StORM.git", .upToNextMinor(from: "3.1.0"))
     ],
     targets: [
         .target(name: "RDM-UIC-Manager", dependencies: ["PerfectHTTPServer","SQLiteStORM"])
